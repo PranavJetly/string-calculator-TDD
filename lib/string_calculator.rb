fbfd406ea73a365d5d660330ab86a10e5db8230d
pranavjetly <admin@Admins-MacBook-Air.local>
2025-09-29 21:50:37 +0530

    Commit 8: Added Support for new line separators

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,7 @@ require 'pry'
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers == ""
+    numbers.gsub!("\n",",")
     chars = numbers.split(',')
     chars.reduce(0){|sum,ch| sum+ch.to_i}
   end
