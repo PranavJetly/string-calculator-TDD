347ef1c64002f282ead526acf0c900ad80cccb68
pranavjetly <admin@Admins-MacBook-Air.local>
2025-09-29 22:22:19 +0530

    Commit 10: Added support to custom validations

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,8 +3,10 @@ require 'pry'
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers == ""
-    numbers.gsub!("\n",",")
-    chars = numbers.split(',')
+    delimeter = numbers.start_with?("//") ? numbers[2] : ","
+    removeline = numbers.start_with?("//") ? "" : ","
+    numbers.gsub!("\n", removeline)
+    chars = numbers.split(delimeter)
     chars.reduce(0){|sum,ch| sum+ch.to_i}
   end
 end
