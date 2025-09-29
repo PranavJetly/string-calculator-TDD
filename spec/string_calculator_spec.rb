55978eb2d2a5972ac98631741db409eb671fc968
pranavjetly <admin@Admins-MacBook-Air.local>
2025-09-29 21:34:28 +0530

    Commit 7: Added test case for '\n' in string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,16 +2,24 @@ require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
-    it 'returns 0 for an empty string' do
+    it "returns 0 for an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
 
-    it 'returns number for string' do
+    it "returns number for string" do
       expect(StringCalculator.add("1")).to eq(1)
     end
 
-    it 'returns summation of two strings' do
+    it "returns summation of two strings" do
       expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+    it "returns sum of multiple numbers in strings" do
+      expect(StringCalculator.add("1,5,5")).to eq(11)
+    end
+
+    it "returns sum of numbers in strings with '\n'" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
   end
 end
