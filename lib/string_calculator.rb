dd23a865e7679bff5e4e92310ec2d6940afa9fe6
pranavjetly <admin@Admins-MacBook-Air.local>
2025-09-30 15:38:40 +0530

    Commit 12: Added support for custom delimiter of any length

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,10 +3,17 @@ require 'pry'
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers == ""
-    delimeter = numbers.start_with?("//") ? numbers[2] : ","
-    removeline = numbers.start_with?("//") ? "" : ","
+    removeline = ","
+    delimiter = ","
+    if numbers.start_with?("//")
+      # split first line and rest
+      delimiter_line, numbers = numbers.split("\n", 2)
+      delimiter = delimiter_line[2..-1]
+      removeline = "" #make removeline empty if string starts with '//''
+    end
+
     numbers.gsub!("\n", removeline)
-    chars = numbers.split(delimeter)
+    chars = numbers.split(delimiter)
     chars.reduce(0){|sum,ch| sum+ch.to_i}
   end
 end
